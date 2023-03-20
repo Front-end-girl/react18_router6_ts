4c8c5c35105877f3e8277d9d66f102706569606d
Rona <17666067919@163.com>
2023-03-20 23:10:05 +0800

    [feat]:添加验证码界面

--- src/view/User/InputPhone/index.tsx
+++ src/view/User/InputPhone/index.tsx
@@ -4,13 +4,15 @@ import NavBar from '@/components/NavBar'
 import { trimPhone } from '@/utils/index'
 
 const maxLength = 4
+let Timer: number
 
 function InputPhone() {
     const [phone, setPhone] = useState<string>('')
     const inputRef = useRef<HTMLInputElement>(null)
-    const sentStatus = useState<boolean>(false)
+    const [sendStatus, setSendStatus] = useState<boolean>(false)
     const [vcode, setVcode] = useState('')
     const [cursorIndex, setCursorIndex] = useState(0)
+    const [timer, setTimer] = useState<number>(60)
     const inputCodeRef = useRef<HTMLInputElement>(null)
 
     const onChangePhone = (e: { target: { value: any } }) => {
@@ -31,7 +33,22 @@ function InputPhone() {
         setVcode(val)
         setCursorIndex(val.length)
     }
+    const getCode = () => {
+        setSendStatus(true)
+        Timer = window.setInterval(() => {
+            // 设置一个函数才能获取当前值
+            setTimer(timer => timer - 1)
+        }, 1000)
+    }
 
+    useEffect(() => {
+        if (timer === 0) {
+            clearInterval(Timer)
+        }
+        return () => {
+            clearInterval(Timer)
+        }
+    }, [timer])
     useEffect(() => {
         // 延迟保证翻页动画生效
         setTimeout(() => {
@@ -43,7 +60,7 @@ function InputPhone() {
     return (
         <div className={styles['input-phone']}>
             <NavBar title="手机号登录"></NavBar>
-            {!sentStatus ? (
+            {!sendStatus ? (
                 <>
                     <div className={`${styles['input-text']} d-flex-center`}>
                         <div>+86</div>
@@ -52,6 +69,7 @@ function InputPhone() {
                     <button
                         className={`${styles['input-btn']} d-flex-center`}
                         disabled={trimPhone(phone).length !== 11}
+                        onClick={getCode}
                     >
                         下一步
                     </button>
@@ -60,7 +78,12 @@ function InputPhone() {
                 <div className={styles['code-styles']}>
                     <div className="d-flex">
                         <div>验证码已发送至</div>
-                        <div>{phone}</div>
+                        <div className={styles['code-phone']}>
+                            {phone.replace(/(\d{3})\s(\d{4})\s(\d{4})/g, '+86 $1 **** $3')}
+                        </div>
+                    </div>
+                    <div className={`d-flex justify-end ${styles['code-timer']}`}>
+                        {timer ? <span>{timer}S</span> : <span>重新发送</span>}
                     </div>
                     <div className={styles['v-code-text']}>验证码</div>
                     <div className={styles['v-code']}>
